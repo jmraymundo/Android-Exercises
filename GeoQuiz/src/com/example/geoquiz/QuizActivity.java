cb597b2a30d1d7fd46a14763cca47c5d3cc58349
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-10 10:10:29 +0800

    Added landscape layout

--- GeoQuiz/src/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/example/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,6 +37,8 @@ public class QuizActivity extends Activity
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
+
+        Log.d( TAG, "onCreate(Bundle) called" );
         setContentView( R.layout.activity_quiz );
 
         myQuestionTextView = ( TextView ) findViewById( R.id.question_text_view );
@@ -87,6 +90,41 @@ public class QuizActivity extends Activity
                 updateQuestion();
             }
         } );
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d( TAG, "onStart() called" );
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d( TAG, "onPause() called" );
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.d( TAG, "onResume() called" );
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.d( TAG, "onStop() called" );
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d( TAG, "onDestroy() called" );
     }
 
     private void checkAnswer( Boolean answer )

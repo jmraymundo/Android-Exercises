38a0d209107052d007884a8a65ca9ddc15abd28c
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-13 11:57:11 +0800

    fixed issue with missing icon

--- CriminalIntent/src/com/example/criminalintent/fragment/CrimeListFragment.java
+++ CriminalIntent/src/com/example/criminalintent/fragment/CrimeListFragment.java
@@ -31,7 +31,6 @@ public class CrimeListFragment extends ListFragment
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        getActivity().setTitle( R.string.crimes_title );
 
         myCrimes = CrimeLab.get( getActivity() ).getCrimes();
 

ca5c59b3e84809afe9028afd2d02155a0bb6e7ac
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-16 17:26:28 +0800

    NerdLauncher now launching apps

--- NerdLauncher/src/com/example/nerdlauncher/fragment/NerdLauncherFragment.java
+++ NerdLauncher/src/com/example/nerdlauncher/fragment/NerdLauncherFragment.java
@@ -1,14 +1,23 @@
 
 package com.example.nerdlauncher.fragment;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import android.R;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class NerdLauncherFragment extends ListFragment
 {
@@ -26,5 +35,47 @@ public class NerdLauncherFragment extends ListFragment
         List< ResolveInfo > activities = pm.queryIntentActivities( startupIntent, 0 );
 
         Log.i( TAG, "I've found " + activities.size() + " activities." );
+        Collections.sort( activities, new Comparator< ResolveInfo >()
+        {
+            @Override
+            public int compare( ResolveInfo lhs, ResolveInfo rhs )
+            {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare( lhs.loadLabel( pm ).toString(),
+                        rhs.loadLabel( pm ).toString() );
+            }
+        } );
+
+        ArrayAdapter< ResolveInfo > adapter = new ArrayAdapter< ResolveInfo >( getActivity(),
+                R.layout.simple_list_item_1, activities)
+        {
+            @Override
+            public View getView( int position, View convertView, ViewGroup parent )
+            {
+                PackageManager pm = getActivity().getPackageManager();
+                View v = super.getView( position, convertView, parent );
+                TextView tv = ( TextView ) v;
+                ResolveInfo ri = getItem( position );
+                tv.setText( ri.loadLabel( pm ) );
+                return v;
+            }
+        };
+        setListAdapter( adapter );
+    }
+
+    @Override
+    public void onListItemClick( ListView listView, View view, int position, long id )
+    {
+        ResolveInfo resolveInfo = ( ResolveInfo ) listView.getAdapter().getItem( position );
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if( activityInfo == null )
+        {
+            return;
+        }
+
+        Intent i = new Intent( Intent.ACTION_MAIN );
+        i.setClassName( activityInfo.applicationInfo.packageName, activityInfo.name );
+        startActivity( i );
     }
 }

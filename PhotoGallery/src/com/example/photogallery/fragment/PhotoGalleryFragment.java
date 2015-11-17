e9a72203970ae52c68f40ed47edf55e8334d1ec4
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-17 17:20:38 +0800

    Added default thumbnail to all photos

--- PhotoGallery/src/com/example/photogallery/fragment/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/example/photogallery/fragment/PhotoGalleryFragment.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class PhotoGalleryFragment extends Fragment
 {
@@ -49,8 +50,7 @@ public class PhotoGalleryFragment extends Fragment
         }
         if( mItems != null )
         {
-            mGridView.setAdapter(
-                    new ArrayAdapter< GalleryItem >( getActivity(), android.R.layout.simple_gallery_item, mItems ) );
+            mGridView.setAdapter( new GalleryItemAdapter( mItems ) );
         }
         else
         {
@@ -72,6 +72,26 @@ public class PhotoGalleryFragment extends Fragment
             mItems = result;
             setupAdapter();
         }
+    }
 
+    private class GalleryItemAdapter extends ArrayAdapter< GalleryItem >
+    {
+        public GalleryItemAdapter( ArrayList< GalleryItem > items )
+        {
+            super( getActivity(), 0, items );
+        }
+
+        @Override
+        public View getView( int position, View convertView, ViewGroup parent )
+        {
+            if( convertView == null )
+            {
+                convertView = getActivity().getLayoutInflater().inflate( R.layout.gallery_item, parent, false );
+            }
+
+            ImageView imageView = ( ImageView ) convertView.findViewById( R.id.gallery_item_imageView );
+            imageView.setImageResource( R.drawable.brian_up_close );
+            return convertView;
+        }
     }
 }

e25cb7af3dd1767f5ec700b55d09ee34587f5d2b
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-17 17:27:14 +0800

    added ThumbnailDownloader.java

---
+++ PhotoGallery/src/com/example/photogallery/utils/ThumbnailDownloader.java
@@ -0,0 +1,20 @@
+
+package com.example.photogallery.utils;
+
+import android.os.HandlerThread;
+import android.util.Log;
+
+public class ThumbnailDownloader< Token > extends HandlerThread
+{
+    private static final String TAG = "ThumbnailDownloader";
+
+    public ThumbnailDownloader()
+    {
+        super( TAG );
+    }
+
+    public void queueThumbnail( Token token, String url )
+    {
+        Log.i( TAG, "Got a URL: " + url );
+    }
+}

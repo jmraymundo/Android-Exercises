e2510cf5ba7cd5931e8fcab125746c37e8a05bf5
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-17 10:30:26 +0800

    First commit

---
+++ RemoteControl/src/com/example/remotecontrol/activity/RemoteControlActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.remotecontrol.activity;
+
+import android.support.v4.app.Fragment;
+
+public class RemoteControlActivity extends SingleFragmentActivity
+{
+    @Override
+    protected Fragment createFragment()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}

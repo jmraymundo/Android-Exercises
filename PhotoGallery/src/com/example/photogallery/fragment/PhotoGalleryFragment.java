89a8a04fea6b2d2c7417a67827104a2a5e4c64ce
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-17 16:05:33 +0800

    Able to connect to google.com

--- PhotoGallery/src/com/example/photogallery/fragment/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/example/photogallery/fragment/PhotoGalleryFragment.java
@@ -1,10 +1,17 @@
 
 package com.example.photogallery.fragment;
 
-import com.example.photogallery.R;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import com.example.photogallery.R;
+import com.example.photogallery.utils.FlickrFetcher;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +21,14 @@ public class PhotoGalleryFragment extends Fragment
 {
     private GridView mGridView;
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setRetainInstance( true );
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -27,5 +37,24 @@ public class PhotoGalleryFragment extends Fragment
         View v = inflater.inflate( R.layout.fragment_photo_gallery, container, false );
         mGridView = ( GridView ) v.findViewById( R.id.gridView );
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask< Void, Void, Void >
+    {
+        @Override
+        protected Void doInBackground( Void... params )
+        {
+            try
+            {
+                String result = new FlickrFetcher().getUrl( "https://www.google.com" );
+                Log.i( TAG, "Fetched contents of URL: " + result );
+            }
+            catch( IOException e )
+            {
+                Log.e( TAG, "Failed to fetch URL: " + e );
+            }
+            return null;
+        }
+
     }
 }
---
+++ PhotoGallery/src/com/example/photogallery/utils/FlickrFetcher.java
@@ -0,0 +1,53 @@
+
+package com.example.photogallery.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class FlickrFetcher
+{
+    public byte[] getUrlBytes( String urlSpec ) throws IOException
+    {
+        URL url = new URL( urlSpec );
+        HttpURLConnection connection = ( HttpURLConnection ) url.openConnection();
+        try
+        {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if( connection.getResponseCode() != HttpURLConnection.HTTP_OK )
+            {
+                return null;
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while( ( bytesRead = in.read( buffer ) ) > 0 )
+            {
+                out.write( buffer, 0, bytesRead );
+            }
+            out.close();
+            byte[] output = out.toByteArray();
+            return output;
+        }
+        finally
+
+        {
+            connection.disconnect();
+        }
+
+    }
+
+    public String getUrl( String urlSpec ) throws IOException
+    {
+        byte[] byteUrl = getUrlBytes( urlSpec );
+        if( byteUrl == null )
+        {
+            return null;
+        }
+        return new String( byteUrl );
+    }
+}

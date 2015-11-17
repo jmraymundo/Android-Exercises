0e11daaaba527af96c3c4511496e8bb242feb0dd
Jan Michael Raymundo <j.raymundo@klab.com>
2015-11-17 11:29:20 +0800

    Added 0 and 1 buttons

--- RemoteControl/src/com/example/remotecontrol/activity/RemoteControlActivity.java
+++ RemoteControl/src/com/example/remotecontrol/activity/RemoteControlActivity.java
@@ -1,14 +1,24 @@
 
 package com.example.remotecontrol.activity;
 
+import com.example.remotecontrol.fragment.RemoteControlFragment;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
 
 public class RemoteControlActivity extends SingleFragmentActivity
 {
     @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        super.onCreate( savedInstanceState );
+    }
+
+    @Override
     protected Fragment createFragment()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return new RemoteControlFragment();
     }
 }
